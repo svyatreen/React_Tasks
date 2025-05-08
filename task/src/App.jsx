94b5c3d273f39026284425fa9014b55f3360659d
Svyatoslav Lazovskiy <svatoslav159753@gmail.com>
2025-05-08 18:18:46 +0300

    rewrite App component without JSX

--- task/src/App.jsx
+++ task/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, createElement } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,35 +8,56 @@ function App() {
 	const [count, setCount] = useState(0);
 	const currentYear = new Date().getFullYear();
 
-	return (
+	return createElement(
 		// Декларативный
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<h1>{currentYear}</h1>
-		</>
+		React.Fragment,
+		null,
+		createElement(
+			"div",
+			null,
+			createElement(
+				"a",
+				{ href: "https://vite.dev", target: "_blank" },
+				createElement("img", {
+					src: viteLogo,
+					className: "logo",
+					alt: "Vite logo",
+				})
+			),
+			createElement(
+				"a",
+				{ href: "https://react.dev", target: "_blank" },
+				createElement("img", {
+					src: reactLogo,
+					className: "logo react",
+					alt: "React logo",
+				})
+			)
+		),
+		createElement("h1", null, "Vite + React"),
+		createElement(
+			"div",
+			{ className: "card" },
+			createElement(
+				"button",
+				{ onClick: () => setCount((count) => count + 1) },
+				"count is ",
+				count
+			),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", null, "src/App.jsx"),
+				" and save to test HMR"
+			)
+		),
+		createElement(
+			"p",
+			{ className: "read-the-docs" },
+			"Click on the Vite and React logos to learn more"
+		),
+		createElement("h1", null, currentYear)
 	);
 }
 
